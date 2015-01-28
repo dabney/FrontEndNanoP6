828ff65c222ceb51efeb3d7c584804dede069772
Dabney Blum <dabneyblum@gmail.com>
2015-01-27 21:08:06 -0600

    menu toggle hide tested

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -54,19 +54,30 @@ $(function() {
 
 
     /* TODO: Write a new test suite named "The menu" */
-
+    describe('The menu', function() {
         /* TODO: Write a test that ensures the menu element is
          * hidden by default. You'll have to analyze the HTML and
          * the CSS to determine how we're performing the
          * hiding/showing of the menu element.
          */
-
-         /* TODO: Write a test that ensures the menu changes
+        it('menu is hidden by default', function() {
+            expect($('body').hasClass('menu-hidden')).toBe(true);
+        });
+        /* TODO: Write a test that ensures the menu changes
           * visibility when the menu icon is clicked. This test
           * should have two expectations: does the menu display when
           * clicked and does it hide when clicked again.
           */
+        it('menu toggles when menu icon clicked', function() {
+            var initialToggleState = $('body').hasClass('menu-hidden');
 
+            $('.menu-icon-link').trigger("click");
+            expect($('body').hasClass('menu-hidden')).not.toBe(initialToggleState);
+
+            $('.menu-icon-link').trigger("click");
+            expect($('body').hasClass('menu-hidden')).toBe(initialToggleState);
+        });
+    });
     /* TODO: Write a new test suite named "Initial Entries" */
 
         /* TODO: Write a test that ensures when the loadFeed
